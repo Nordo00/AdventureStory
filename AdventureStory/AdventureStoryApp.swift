ab00ed30075ef2d5e597337b77ccd6f694417088
Nordo <N/A>
2023-03-16 10:52:20 -0400

    Initial Commit

---
+++ AdventureStory/AdventureStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureStoryApp.swift
+//  AdventureStory
+//
+//  Created by Nordo on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AdventureStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
